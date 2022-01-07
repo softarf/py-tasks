eca4bca04e52d5062f596279520d4937c17a830f
arfweb <84576938+arfweb@users.noreply.github.com>
2022-01-07 21:42:54 +0500

    Представляет первую задачу из Д.З. 3 в альтернативном варианте
    В операции распаковки for заменяет Список pair[] на две Переменные boy и girl,
    что будет соответствовать двум Аргументам функции zip.
    Запись с явным указанием элементов Понятнее, поэтому Приветствуется.
    Использование Списка pair[] удобнее для переменного числа Аргументов.

--- task_1.3_01.py
+++ task_1.3_01.py
@@ -6,8 +6,16 @@ boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
 girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
 if len(boys) == len(girls):
     print('\n Идеальные пары:')
+    '''
+    # Универсальный вариант: список pair[] удобен для переменного числа аргументов функции zip
     for pair in zip(sorted(boys), sorted(girls)):
         print(f' {pair[0]} и {pair[1]}')
+    '''
+    #   Альтернативный вариант: список pair[] распакован на две переменные boy и girl,
+    #   соответственно, для двух аргументов функции zip.
+    #   Запись с явным указанием элементов Понятнее, поэтому Приветствуется.
+    for boy, girl in zip(sorted(boys), sorted(girls)):
+        print(f' {boy} и {girl}')
 else:
     print('\n Кто-то может остаться без пары!')
 
