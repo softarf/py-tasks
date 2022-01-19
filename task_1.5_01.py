e1d131fa1ad3063a2e5e6084f62d7f72373d42af
arfweb <84576938+arfweb@users.noreply.github.com>
2022-01-20 01:12:18 +0500

    Заканчивает работу над 1-ой задачей из ДЗ 5 и переходит на 2-ю
    Добавляет функцию "Создание нового документа".
    
    Начанает работу над второй задачей из ДЗ 5.
    А именно: Добавляет функцию "Удаление документа".
    Функция "Перенос на новую полку" пока не получается.
    Ещё останется функция "Создание новой полки".

--- task_1.5_01.py
+++ task_1.5_01.py
@@ -1,17 +1,20 @@
 
 # coding: utf-8        # -*- coding: utf-8 -*-
 
-def pr_queries(my_docs, my_dirs):
-	print('\n Исходные данные:\n documents = [', end='')
-	idx = 0
-	for document in my_docs:
-		idx += 1
-		if idx != 1:
-			print(f',\n     {document}', end='')
-		else:
-			print(f'\n     {document}', end='')
-	print('\n    ]')
-	print(' directories = {', end='')
+def print_docs(my_docs):
+    print(' Документы: documents = [', end='')
+    idx = 0
+    for document in my_docs:
+        idx += 1
+        if idx != 1:
+            print(f',\n     {document}', end='')
+        else:
+            print(f'\n     {document}', end='')
+    print('\n    ]')
+    return
+
+def print_dirs(my_dirs):
+	print(' Полки: directories = {', end='')
 	idx = 0
 	for dir_num, dir_content in my_dirs.items():
 		idx += 1
@@ -37,10 +40,10 @@ def desiring_person(my_docs):
 def desiring_shelf(my_dirs):
 	indicate = True
 	num_doc = input('\n Введите номер документа: ')
-	for doc_key in my_dirs.keys():
-		if num_doc in my_dirs[doc_key]:
+	for num_shelf in my_dirs.keys():
+		if num_doc in my_dirs[num_shelf]:
 			indicate = False
-			print(f" Искомая полка: '{doc_key}'")
+			print(f" Искомая полка: '{num_shelf}'")
 			break
 	if indicate:
 		print(f"\n Нет документа с номером '{num_doc}'")
@@ -53,35 +56,125 @@ def listing_docs(my_docs):
 		for key in document.keys():
 			idx += 1
 			if idx != 1:
-				print(f' "{document[key]}"', end=' ')
+				print(f' "{document[key]}" ', end=' ')
 			else:
-				print(f' {document[key]}', end=' ')
+				print(f' {document[key]} ', end=' ')
 		print()
 	return
 		
 def create_new_doc(my_docs, my_dirs):
 	indicate = True
-	print('\n Создание нового документа (q - отмена):')
+	print('\n Создание нового документа (q - отмена).')
 	my_type = input(' Введите тип документа: ')
-	if my_type.lower() != 'q' and indicate:
-		pass
-	else:
-		indicate = False
-	my_num = input(' Введите номер документа: ')
-	if my_type.lower() != 'q' and indicate:
-		pass
-	else:
-		indicate = False
-	my_name = input(' Введите имя: ')
-	if my_type.lower() != 'q' and indicate:
-		pass
-	else:
-		indicate = False
-	my_shelf = input(' Введите номер полки: ')
-	if my_type.lower() != 'q' and indicate:
-		pass
-	else:
-		indicate = False
+	if my_type.strip().lower() != 'q':
+		my_num = input(' Введите номер документа: ')
+		if my_num.strip().lower() != 'q':
+			my_name = input(' Введите имя: ')
+			if my_name.strip().lower() != 'q':
+				bool_while = True
+				while bool_while:
+					my_shelf = input(' Введите номер полки: ')
+					if my_shelf.strip().lower() != 'q':
+						if my_shelf in my_dirs.keys():
+							new_doc = {"type": my_type, "number": my_num, "name": my_name}
+							my_docs.append(new_doc)
+							my_dirs[my_shelf].append(new_doc["number"])
+							print('\n Запись добавлена')
+							indicate = False
+							bool_while = False
+						else:
+							print('\n Нет такой полки')
+					else:
+						bool_while = False
+	if indicate:
+		print('\n Операция отменена')
+	# pr_queries(my_docs, my_dirs)  # Для теста
+	return
+			
+def delete_doc(my_docs, my_dirs):
+	print('\n Удаление старого документа (q - отмена).')
+	while_num = True
+	while while_num:
+		my_num = input(' Введите номер документа: ')
+		if my_num.strip().lower() != 'q':
+			indicate = True
+			for idx, doc in enumerate(my_docs):
+				if my_num == doc["number"]:
+					del my_docs[idx]
+					for key in my_dirs:
+						if my_num in my_dirs[key]:
+							my_dirs[key].remove(my_num)
+							break
+					print(f"\n Документ '{my_num}' удалён")
+					indicate = False
+					while_num = False
+					break
+			if indicate:
+				print('\n Нет такого документа')
+		else:
+			print('\n Операция отменена')
+			while_num = False
+	return
+	
+def change_dir(my_docs, my_dirs):
+	print('\n Перенос документа на другую полку (q - отмена).')
+	while_num = True
+	while while_num:
+		num_doc = input(' Введите номер документа: ')
+		if num_doc.strip().lower() != 'q':
+			ind_num = True
+			for doc in my_docs:
+				if num_doc == doc["number"]:
+					ind_num = False
+					for old_shelf in my_dirs.keys():
+						if num_doc in my_dirs[old_shelf]:
+							indicate = False
+							print(f" Искомая полка: '{old_shelf}'")
+							break
+					while_dir = True
+					while while_dir:
+						new_shelf = input(' Введите номер полки: ')
+						if new_shelf.strip().lower() != 'q':
+							ind_dir = True
+							if new_shelf in my_dirs.keys():
+
+								# Удаляем из старой полки
+								# Заносим на новую полку
+								pass
+							else:
+								print('\n Нет такой полки')
+						else:
+							# print('\n Операция отменена')
+							while_dir = False
+							while_num = False
+					###
+					for key in my_dirs:
+						if num_doc in my_dirs[key]:
+							my_dirs[key].remove(num_doc)
+							break
+					print(f"\n Документ '{num_doc}' удалён")
+					indicate = False
+					while_num = False
+					break
+					###
+				else:
+					print('\n Нет такого документа')
+		else:
+			print('\n Операция отменена')
+			while_num = False
+	return
+									###
+def create_new_shelf(my_dirs):
+	print('\n Перенос документа на другую полку (q - отмена).')
+	indicate = True
+	num_doc = input(' Введите номер документа: ')
+	for doc_key in my_dirs.keys():
+		if num_doc in my_dirs[doc_key]:
+			indicate = False
+			print(f" Искомая полка: '{doc_key}'")
+			break
+	if indicate:
+		print(f"\n Нет документа с номером '{num_doc}'")
 	return
 
 
@@ -97,12 +190,16 @@ directories = {
         '3': []
       }
 
-pr_queries(documents, directories)
+print('\n Исходные данные:')
+print_docs(documents)
+print_dirs(directories)
 
 bool_repeat = True
 while bool_repeat:
-	print('\n Доступные команды:\n     p - Чей документ\n     s - Какая полка \
-	\n     l - Полный список\n     a - Добавить новый документ\n     q - Выход')
+	print('\n Доступные команды:\n     p  - Чей документ\n     s  - Какая полка \
+	\n     l  - Полный список документов\n     a  - Добавить новый документ \
+	\n     d  - Удалить старый документ\n     m  - Переместить\
+	документ на другую полку\n     as - Добавить новую полку\n     q  - Выход')
 	assign = input('\n Ваш выбор: ')
 	if assign == 'p':
 		desiring_person(documents)
@@ -112,9 +209,14 @@ while bool_repeat:
 		listing_docs(documents)
 	elif assign == 'a':
 		create_new_doc(documents, directories)
+	elif assign == 'd':
+		delete_doc(documents, directories)
+	elif assign == 'm':
+		change_dir(documents, directories)
+	elif assign == 'as':
+		create_new_shelf(directories)
 	elif assign == 'q':  # quite_work 
 		bool_repeat = False
-		print(' Вы ввели:', assign)
 	else:
 		print(f" Неизвестная команда: '{assign}'")
 	if bool_repeat:
@@ -125,4 +227,4 @@ while bool_repeat:
 
 print('\n -- Конец --')
 
-input('\n  -- Конец --  ')	#	Типа  "Пауза" - Для среды
+# input('\n  -- Конец --  ')	#	Типа  "Пауза" - Для среды
